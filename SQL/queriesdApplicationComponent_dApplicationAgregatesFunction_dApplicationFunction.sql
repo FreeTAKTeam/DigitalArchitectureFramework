2ed324e21c8df6403088c204d0d7eb9cacd10b3a
Giu Platania <giu.platania@doorbusinessConsulting.com>
2022-12-09 11:31:21 -0400

    Update use case feature

--- SQL/queriesdApplicationComponent_dApplicationAgregatesFunction_dApplicationFunction.sql
+++ SQL/queriesdApplicationComponent_dApplicationAgregatesFunction_dApplicationFunction.sql
@@ -1,9 +1,9 @@
 -- Generated 10/24/2022 2:11:48 PM
 --  dApplicationComponent (GroupName) connected with  dApplicationFunction (series)
-SELECT dApplicationComponent.Name as dApplicationComponent,  dApplicationFunction.Name as  dApplicationFunction
-FROM t_object AS dApplicationComponent
-INNER JOIN t_connector as connector ON source.Object_ID = connector.Start_Object_ID
-INNER JOIN t_object AS target ON connector.End_Object_ID =  dApplicationFunction.Object_ID
-WHERE dApplicationComponent.Stereotype='dApplicationFunction'
-AND  dApplicationFunction.Stereotype='dApplicationComponent'
+SELECT ApplicationComponent.Name as ApplicationComponent,  ApplicationFunction.Name as  ApplicationFunction, ApplicationFunction.Note as description
+FROM t_object AS ApplicationComponent
+INNER JOIN t_connector as connector ON ApplicationComponent.Object_ID = connector.End_Object_ID
+INNER JOIN t_object AS ApplicationFunction ON connector.Start_Object_ID =  ApplicationFunction.Object_ID
+WHERE ApplicationComponent.Stereotype='dApplicationComponent'
+AND  ApplicationFunction.Stereotype='dApplicationFunction'
 AND connector.Stereotype='dApplicationAgregatesFunction'
--- SQL/queriesdBusinessUseCase_is realized by Use Case_dFeature.sql
+++ SQL/queriesdBusinessUseCase_is realized by Use Case_dFeature.sql
@@ -7,3 +7,13 @@ INNER JOIN t_object AS target ON connector.End_Object_ID =  dFeature.Object_ID
 WHERE dBusinessUseCase.Stereotype='dFeature'
 AND  dFeature.Stereotype='dBusinessUseCase'
 AND connector.Stereotype='is realized by Use Case'
+
+
+SELECT dBusinessUseCase.Name as dBusinessUseCase,  dApplicationComponent.Name as  dApplicationComponent
+FROM t_object AS dBusinessUseCase
+INNER JOIN t_connector as connector ON dBusinessUseCase.Object_ID = connector.End_Object_ID
+INNER JOIN t_object AS dApplicationComponent ON connector.Start_Object_ID =  dApplicationComponent.Object_ID
+WHERE dBusinessUseCase.Stereotype='dApplicationComponent'
+AND  dApplicationComponent.Stereotype='dBusinessUseCase'
+AND connector.type='realization'
+AND dBusinessUseCase.version = 2.0
